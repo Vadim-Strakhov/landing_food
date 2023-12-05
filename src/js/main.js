a5955d123536c98545f133f4ae3233987a190ec3
Vadim-Strakhov <mars1597@gmail.com>
2023-12-05 17:35:23 +0300

    adding timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
+'use strict';
+
 window.addEventListener('DOMContentLoaded', () => {
+  // // Tabs
+
   const tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent'),
     tabsParent = document.querySelector('.tabheader__items');
 
   function hideTabContent() {
     tabsContent.forEach((item) => {
-      // item.style.display = 'none'; // ? вариант с inline стилями
+      // item.style.display = 'none'; //_ Вариант с inline стилями
       item.classList.add('hide');
       item.classList.remove('show', 'fade');
     });
@@ -16,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   function showTabContent(i = 0) {
-    // tabsContent[i].style.display = 'block'; // ? вариант с inline стилями
+    // tabsContent[i].style.display = 'block'; //_ Вариант с inline стилями
     tabsContent[i].classList.add('show', 'fade');
     tabsContent[i].classList.remove('hide');
     tabs[i].classList.add('tabheader__item_active');
@@ -37,4 +41,81 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  // // Timer
+
+  // const deadline = '2023-12-10'; //_ Точное значение даты
+
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate() + 1).padStart(2, '0');
+  const formattedDate = `${year}-${month}-${day}`; //_ Значение до конца текущего дня
+
+
+  const deadline = formattedDate;
+
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+
+    //_ Условие для обработки прошедшей даты, если указана конкретная дата
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      (days = Math.floor(t / (1000 * 60 * 60 * 24))),
+        (hours = Math.floor((t / (1000 * 60 * 60)) % 24)),
+        (minutes = Math.floor((t / 1000 / 60) % 60)),
+        (seconds = Math.floor((t / 1000) % 60));
+    }
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  //_ Функция для добавления 0 в однозначные числа в таймере
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock(); //_  Для моментального запуска таймера на странице
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadline);
 });
