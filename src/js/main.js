8e66e0daf6c7e48a34de1b0e0488f59b3db86c27
Vadim-Strakhov <mars1597@gmail.com>
2023-12-08 09:14:32 +0300

    adding modal

--- src/js/main.js
+++ src/js/main.js
@@ -52,17 +52,14 @@ window.addEventListener('DOMContentLoaded', () => {
   const day = String(currentDate.getDate() + 1).padStart(2, '0');
   const formattedDate = `${year}-${month}-${day}`; //_ Значение до конца текущего дня
 
-
   const deadline = formattedDate;
 
   function getTimeRemaining(endtime) {
     let days, hours, minutes, seconds;
     const t = Date.parse(endtime) - Date.parse(new Date());
 
-
-    //_ Условие для обработки прошедшей даты, если указана конкретная дата
-
     if (t <= 0) {
+      //_ Условие для обработки прошедшей даты и показа 0, если указана конкретная дата
       days = 0;
       hours = 0;
       minutes = 0;
@@ -83,9 +80,8 @@ window.addEventListener('DOMContentLoaded', () => {
     };
   }
 
-  //_ Функция для добавления 0 в однозначные числа в таймере
-
   function getZero(num) {
+    //_ Функция для добавления 0 в однозначные числа в таймере на странице
     if (num >= 0 && num < 10) {
       return `0${num}`;
     } else {
@@ -118,4 +114,70 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   setClock('.timer', deadline);
+
+  // // Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+  function closeModal() {
+    modal.classList.toggle('show'); //_ Вариант с toggle
+    // modal.classList.add('hide');  //_ Вариант с классами
+    // modal.classList.remove('show');
+    document.body.style.overflow = ''; //_ Для включения scroll
+  }
+
+  function openModal() {
+    modal.classList.toggle('show'); //_ Вариант с toggle
+    // modal.classList.add('show'); //_ Вариант с классами
+    // modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden'; //_ Для отключения scroll
+    clearInterval(modalTimerId); //_ Отключение таймера для открытия окна, чтобы оно не появилось опять, если уже было открыто
+  }
+
+  modalTrigger.forEach((btn) => {
+    //_ Открытие модального окна
+    btn.addEventListener('click', () => {
+      openModal();
+    });
+  });
+
+  modalCloseBtn.addEventListener('click', closeModal); //_ Закрытие модального окна на крестик
+
+  modal.addEventListener('click', (e) => {
+    //_ Закрытие модального окна кликом на пустую область
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    //_ Закрытие модального окна клавишей Esc, если оно открыто
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 5000); //_ Открытие модального окна через 3 секунды на сайте
+
+  function showModalByScroll() {
+    //_ Открытие модального окна после прокрутки всей страницы
+
+    if (
+      window.scrollY + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll); //_ Удаление обработчика события, чтобы окно не открывалось повторно
+    }
+
+    //_ Вариант от ChatGPT
+    // if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 1) {
+    //   openModal();
+    //   window.removeEventListener('scroll', showModalByScroll);
+    // }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
